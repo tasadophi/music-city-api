bfb58ed32be5eca91d6b830fbba83756cdc7e184
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2023-08-31 00:02:05 +0330

    feat: add data

---
+++ data/artistsWithId.js
@@ -0,0 +1,363 @@
+module.exports = artistsWithId = [
+  {
+    name: "Shadmehr",
+    followers: 38.4,
+    imageUrl:
+      "https://i1.sndcdn.com/artworks-g8QnuYJy4X8J7MIP-Ou7mmw-t500x500.jpg",
+    genre: "pop",
+    id: "59dc772c74f446f6809b125e6d530e2c",
+  },
+  {
+    name: "Mohsen Chavoshi",
+    followers: 32.1,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63f111172a85a.jpg",
+    genre: "pop",
+    id: "0b014983ec169657b5dadbf1d2f82e74",
+  },
+  {
+    name: "Mohsen Yeganeh",
+    followers: 30.8,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5eb8c27c4496f4596cfef8f8359",
+    genre: "pop",
+    id: "2bbc3b69dad91e43c025b5702f6a5faf",
+  },
+  {
+    name: "Mohsen Ebrahimzadeh",
+    followers: 33.9,
+    imageUrl:
+      "https://nicmusic.net/wp-content/uploads/2023/05/Mohsen-Ebrahimzade-Remix.jpg",
+    genre: "pop",
+    id: "d60731f0b39987e4bd188103c56c1272",
+  },
+  {
+    name: "Reza Bahram",
+    followers: 33.3,
+    imageUrl:
+      "https://mahanmusic.net/wp-content/uploads/2021/08/Reza-Bahram-Shahr-Khali-mahanmusic.net_.jpg",
+    genre: "pop",
+    id: "e58772fcfeb91ed046232b488b02433f",
+  },
+  {
+    name: "Homayoun Shajarian",
+    followers: 20.2,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5ebbba22d25abdcfdde889ed7f2",
+    genre: "pop",
+    id: "4312cdf3ef87637a686adcf1ba9e9103",
+  },
+  {
+    name: "Babak Jahanbakhsh",
+    followers: 19.8,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63efdff4dd6aa.jpg",
+    genre: "pop",
+    id: "5b18cff0e0ba77ebbb2a40dc375727b5",
+  },
+  {
+    name: "Reza Sadeghi",
+    followers: 17,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63f0f3b7e6bb2.jpg",
+    genre: "pop",
+    id: "71171dc488385266a666b4612e94113d",
+  },
+  {
+    name: "Mehdi Ahmadvand",
+    followers: 29.1,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63f10f183983a.jpg",
+    genre: "pop",
+    id: "81e2484cbb9db53969ed03595b562599",
+  },
+  {
+    name: "Alireza Talischi",
+    followers: 27.7,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63ee06351c1fd.jpg",
+    genre: "pop",
+    id: "fc7a8235ccc3f0241965168239307cd5",
+  },
+  {
+    name: "Farzad Farzin",
+    followers: 23.8,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5ebcfa64a9406760f132ca7695d",
+    genre: "pop",
+    id: "169543e8b538efb640ed186766a4f8e8",
+  },
+  {
+    name: "Hoorosh Band",
+    followers: 24.8,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63ee08737f629.jpg",
+    genre: "pop",
+    id: "a78daad3446b88c7aa830a5b5bd54375",
+  },
+  {
+    name: "Hamid Hirad",
+    followers: 25,
+    imageUrl:
+      "https://yt3.googleusercontent.com/VfJa-rh_g3_T9oMgs03Wt0xze0kDb-YJomtXu_m_56BaUKamcEx4xSz-xNQ3K3mGZJOuPkfvviA=s900-c-k-c0x00ffffff-no-rj",
+    genre: "pop",
+    id: "fe9518c2e3faac1cc8dc7e123da29abc",
+  },
+  {
+    name: "Ali Yasini",
+    followers: 24.3,
+    imageUrl:
+      "https://yt3.googleusercontent.com/V0KLvxreqULB6pGrkJLa_ZwQTHnzPn-vNqYmEG9-gFkhUpbqCb28aXuoU_suYo4L2V2ywbv2=s900-c-k-c0x00ffffff-no-rj",
+    genre: "pop",
+    id: "8beb8c0a6d7692238ec1fb33fa7dbca8",
+  },
+  {
+    name: "Meysam Ebrahimi",
+    followers: 20.7,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63f131b68d424.jpg",
+    genre: "pop",
+    id: "a9e12ab458f91b1ca91310565eb3c93d",
+  },
+  {
+    name: "Macan Band",
+    followers: 24.9,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63ee07e3f3a41.jpg",
+    genre: "pop",
+    id: "12ca8713cdbaca4ea9801d0dca05e113",
+  },
+  {
+    name: "Ashvan",
+    followers: 18.7,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63efddd0182d5.jpg",
+    genre: "pop",
+    id: "cd74cfbc6159260caceafa4bf32bedf7",
+  },
+  {
+    name: "Yas",
+    followers: 39.7,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5eb32fbc830349cd7bb780d2381",
+    genre: "rap",
+    id: "1999d46f6d236c359c4b86c7559beed0",
+  },
+  {
+    name: "Shayea",
+    followers: 29.9,
+    imageUrl:
+      "https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/7a/87/44/7a874457-6d2e-99d5-2359-63383c99c80a/pr_source.png/486x486bb.png",
+    genre: "rap",
+    id: "5c842087304f7a56c07a298ecef654c2",
+  },
+  {
+    name: "Mehrad Hidden",
+    followers: 32.1,
+    imageUrl: "https://i1.sndcdn.com/artworks-000493624422-0u4ksu-t500x500.jpg",
+    genre: "rap",
+    id: "5a10b9637f26cb38a51939fa977513be",
+  },
+  {
+    name: "Ho3ein",
+    followers: 25.3,
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/6/65/Ho3ein_in_2020.jpg",
+    genre: "rap",
+    id: "9ec5e08c351e866ae3672846476e66c9",
+  },
+  {
+    name: "Amir Khalvat",
+    followers: 7.3,
+    imageUrl:
+      "https://images.genius.com/c071a959a23ae248f9840b7ecc62c671.693x693x1.jpg",
+    genre: "rap",
+    id: "ea3d5c2a69a90297472816f83d01aa7c",
+  },
+  {
+    name: "Reza Pishro",
+    followers: 16.7,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/ar0/718e23e8e1d612a5ee10b105b2c12b35.jpg",
+    genre: "rap",
+    id: "40b0d7db80cee58f5033503928f629b0",
+  },
+  {
+    name: "Hayedeh",
+    followers: 18.5,
+    imageUrl:
+      "https://www.appahang1.com/cdn/tracks-cover/1394/05/7166164745820264381647458202503416474582025267.jpg",
+    genre: "nostalgic",
+    id: "c6dfe28658e183e75ab99382d63ed07b",
+  },
+  {
+    name: "Homeyra",
+    followers: 8.8,
+    imageUrl: "https://i1.sndcdn.com/artworks-000054552020-r57keq-t500x500.jpg",
+    genre: "nostalgic",
+    id: "7ccec7fe5f46f130b81df8433535de44",
+  },
+  {
+    name: "Moein",
+    followers: 25.3,
+    imageUrl:
+      "https://is1-ssl.mzstatic.com/image/thumb/Features115/v4/ce/28/08/ce28082e-b693-24a8-0cc0-16192000c3ff/pr_source.png/486x486bb.png",
+    genre: "nostalgic",
+    id: "b43097a5adfc7b0990d1314fd264a65e",
+  },
+  {
+    name: "Mahasti",
+    followers: 11.5,
+    imageUrl:
+      "https://i.scdn.co/image/ab676161000051749ade2085383f6b1d48b9ff77",
+    genre: "nostalgic",
+    id: "c4be493edda843541ef25be083fa6494",
+  },
+  {
+    name: "Javad Yasari",
+    followers: 21.5,
+    imageUrl:
+      "https://musicsbaran.ir/wp-content/uploads/2020/04/Javad-Yassari.jpg",
+    genre: "nostalgic",
+    id: "067910a182ba26a08698a6f1c2760a75",
+  },
+  {
+    name: "Siavash Ghomeyshi",
+    followers: 21.3,
+    imageUrl: "https://i1.sndcdn.com/artworks-000530304441-jyn82q-t500x500.jpg",
+    genre: "nostalgic",
+    id: "956c8b24ec3d9f891181422459db3a5f",
+  },
+  {
+    name: "Googoosh",
+    followers: 9.7,
+    imageUrl:
+      "https://yt3.googleusercontent.com/ytc/AOPolaSIrWW9TlLKUKgB0OhkhxGs6jyd2UzlfXbDAupj=s900-c-k-c0x00ffffff-no-rj",
+    genre: "nostalgic",
+    id: "28ee929409609ecaa7514181c92f1e9a",
+  },
+  {
+    name: "Leila Forouhar",
+    followers: 5.5,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5eb2a859fb8d1112a40a9148e5a",
+    genre: "nostalgic",
+    id: "4f4791f6ca41c2f95acd9db250d51264",
+  },
+  {
+    name: "Shohreh Solati",
+    followers: 11.9,
+    imageUrl:
+      "https://d1dy244g59v5jo.cloudfront.net/artist-0f/0f16dc6155101f9650372f291a46431f6a7df26a5352b166a36a9050633c23cfb0f6749a.png.512x512.png",
+    genre: "nostalgic",
+    id: "77301bc677cf8d3b56d3c982d1601f5d",
+  },
+  {
+    name: "Sahram Solati",
+    followers: 13.9,
+    imageUrl:
+      "https://popnable.com/images/singers/temp/shahram_solati_iran_top_40_108.jpg",
+    genre: "nostalgic",
+    id: "8d17dc9b48506cb5c338e5aec5f26181",
+  },
+  {
+    name: "Hasan Shamaizadeh",
+    followers: 3.8,
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTDcPk1wkU32iQVD2EcJtqYHGFw0cyYCrLoKx30QwSa_F6USURl",
+    genre: "nostalgic",
+    id: "50e9ce2a20ef7b9c19b28a3f459215ef",
+  },
+  {
+    name: "Omid",
+    followers: 8.1,
+    imageUrl:
+      "https://lastfm.freetls.fastly.net/i/u/ar0/fc6db0687b5a8366e6e6bf0dabb3bf08.jpg",
+    genre: "nostalgic",
+    id: "fbd0ce06f5f6f563cde1e50beb0c5421",
+  },
+  {
+    name: "Mohammadreza Shajarian",
+    followers: 10.5,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5ebf117ed676e04f18767d3b4a7",
+    genre: "sonnati",
+    id: "78c1b4949994401f8e73f7da53d653e8",
+  },
+  {
+    name: "Homayoun Shajarian",
+    followers: 20.2,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5ebbba22d25abdcfdde889ed7f2",
+    genre: "sonnati",
+    id: "1567c9b6e14cfc6d22978cd00ebf9e34",
+  },
+  {
+    name: "Alireza Ghorbani",
+    followers: 9.3,
+    imageUrl:
+      "https://i1.sndcdn.com/avatars-wfXwf3Evuc8yZMRC-trHLTA-t500x500.jpg",
+    genre: "sonnati",
+    id: "f94002686c9b4847b3dcf56f6c534396",
+  },
+  {
+    name: "Alireza Eftekhari",
+    followers: 3.5,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63f10d8de7fe1.jpg",
+    genre: "sonnati",
+    id: "0d663884e5d55f731c1be7fe736bcda1",
+  },
+  {
+    name: "Iraj Bastami",
+    followers: 1.4,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63a9ac18928f7.jpg",
+    genre: "sonnati",
+    id: "5411f9627d7f41258b01ca598b76a0be",
+  },
+  {
+    name: "Haamim",
+    followers: 7.5,
+    imageUrl:
+      "https://i1.sndcdn.com/artworks-ViPTZS4v2y3jUtRp-JAgWLw-t500x500.jpg",
+    genre: "newPop",
+    id: "5c47ad54f104a44cb50f6fedd884dfdf",
+  },
+  {
+    name: "Majid Razavi",
+    followers: 9,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5ebd30a6c298d8c94c7644eec13",
+    genre: "newPop",
+    id: "8f62abd00a9397e4ae0b6f14f24921c6",
+  },
+  {
+    name: "Asef Aria",
+    followers: 10.5,
+    imageUrl:
+      "https://assets.haminlahzeh.com/artists/pop/iran/asef%20aria/profile/picture/avatar/asef%20aria.jpg",
+    genre: "newPop",
+    id: "a55359ece914d4ac95f355b8cef42270",
+  },
+  {
+    name: "Fardin Naji",
+    followers: 1,
+    imageUrl:
+      "https://yt3.googleusercontent.com/l187uk58Ji3qwvPEs8MurPBH4aq_Chrxx1J155IntSHEk_RYSSusWVlPAs5IwTeZX12T350lgQ=s900-c-k-c0x00ffffff-no-rj",
+    genre: "newPop",
+    id: "5876644f8ee19d89aff5e136eb5df637",
+  },
+  {
+    name: "Ragheb",
+    followers: 11.9,
+    imageUrl:
+      "https://i.scdn.co/image/ab6761610000e5eb4a89cb6760bdf8a41a658c29",
+    genre: "newPop",
+    id: "fd387cf72be0f4b8c33f07b8a6e48cb5",
+  },
+  {
+    name: "Sina Derakhshandeh",
+    followers: 11.8,
+    imageUrl: "https://cdnmrtehran.ir/media/artists/63f27ba0d59fa.jpg",
+    genre: "newPop",
+    id: "21c78e665236a4a6cb75a6367d900477",
+  },
+  {
+    name: "Kasra Zahedi",
+    followers: 6.3,
+    imageUrl:
+      "https://yt3.googleusercontent.com/AbM4P6_o6KxU9gaYy8XxVWpNSdcQAiEPB5Yvm65P0A4YGlbc0rhqAyGZV_LYS8EnLFPsf9Mwgvg=s900-c-k-c0x00ffffff-no-rj",
+    genre: "newPop",
+    id: "6a7c97eca0b1985a6aa26559167bbc7c",
+  },
+];
