603f1f1e4d38ddc19ebf8bd1f2ab3c4326a45147
Ali Mahmoudi <alimahmoodi3828@gmail.com>
2023-08-31 00:02:48 +0330

    feat: init server and some routes

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const tracksWithId = require("./data/tracksWithId");
+const artistsWithId = require("./data/artistsWithId");
+const playListsWithId = require("./data/playListsWithId");
+
+const app = express();
+const port = 8000;
+
+app.get("/tracks", (req, res) => {
+  res.status(200).json({
+    data: tracksWithId,
+  });
+});
+
+app.get("/track/:id", (req, res) => {
+  const track = tracksWithId.find((track) => track.id === req.params.id);
+  if (track) res.status(200).json(track);
+  else res.status(404).json({ message: "track not found" });
+});
+
+app.get("/artists", (req, res) => {
+  res.status(200).json({
+    data: artistsWithId,
+  });
+});
+
+app.get("/artist/:id", (req, res) => {
+  const artist = artistsWithId.find((artist) => artist.id === req.params.id);
+  if (artist) res.status(200).json(artist);
+  else res.status(404).json({ message: "artist not found" });
+});
+
+app.get("/playlists", (req, res) => {
+  res.status(200).json({
+    data: playListsWithId,
+  });
+});
+
+app.get("/playlist/:id", (req, res) => {
+  const playList = playListsWithId.find(
+    (playlist) => playlist.id === req.params.id
+  );
+  if (playList) res.status(200).json(playList);
+  else res.status(404).json({ message: "playlist not found" });
+});
+
+app.listen(port, () => {
+  console.log(`music app listening on port ${port}`);
+});
